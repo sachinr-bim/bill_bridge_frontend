5433db5cc7c49021cc50446f91fcaef41640eb08
sachinr-bim <sachinr@bimbasree.in>
2025-05-04 16:21:33 +0530

    Updates

---
+++ assets/icons/CalendarIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CalendarIcon() {
+  return (
+    <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+        <path d="M8 7V3m8 4V3M3 11h18M5 5h14a2 2 0 012 2v12a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2z" />
+    </svg>
+  )
+}
---
+++ assets/icons/ChevronDownIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChevronDownIcon() {
+  return (
+    <svg className="w-4 h-4 ml-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+        <path d="M6 9l6 6 6-6" />
+    </svg>
+  )
+}
