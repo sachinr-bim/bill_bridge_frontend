6e8a32f4aba238c74afc59d633cca05e7b4a94df
sachinr-bim <sachinr@bimbasree.in>
2025-05-05 18:02:00 +0530

    Add Tailwind CSS setup and update dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { store } from './reduxToolkit/store.js'
 import { Provider } from 'react-redux'
 import './index.css'
@@ -9,9 +9,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
